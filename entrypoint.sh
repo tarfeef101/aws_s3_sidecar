112b253e88c93cfd0e238e31b1c29f7e8f4970a0
tarfeef101 <tdedhar@gmail.com>
2021-03-28 00:50:50 -0700

    fix misnamed basename flag in script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ OLDIFS=$IFS
 IFS='|'
 for each in $FILES; do
   aws s3 cp s3://$BUCKET/$each /opt/$each
-  if [ $FLAG -eq 1 ]; then
+  if [ $BASENAME -eq 1 ]; then
     mv /opt/$each /opt/$(basename $each)
   fi
 done
