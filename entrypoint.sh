3347492a7e1bb15194f4e6461360c28c816cb5be
Tareef D <17664517+tarfeef101@users.noreply.github.com>
2026-09-20 17:59:43 -0700

    add recurring build action with test step (#1)
    - weekly GH action that builds, tags {alpine-version}-{awscli-version}, and pushes (skips if tag exists)
    - entrypoint supports NO_SIGN_REQUEST=1 for --no-sign-request against public objects
    - test compose stack pulls a public S3 object and verifies it is served over HTTP
    - README: versioning and testing sections

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+NO_SIGN_FLAG=""
+if [ "$NO_SIGN_REQUEST" = "1" ]; then
+  NO_SIGN_FLAG="--no-sign-request"
+fi
 OLDIFS=$IFS
 IFS='|'
 for each in $FILES; do
-  aws s3 cp s3://$BUCKET/$each /opt/$each
+  aws s3 cp $NO_SIGN_FLAG s3://$BUCKET/$each /opt/$each
   if [ $BASENAME -eq 1 ]; then
     mv /opt/$each /opt/$(basename $each)
   fi
